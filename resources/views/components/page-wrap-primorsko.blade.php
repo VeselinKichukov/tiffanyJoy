e47ec51aee02a9a660bc211999c241470f2566a7
VeselinKichukov <veskichukov@gmail.com>
2018-11-11 15:53:20 +0100

    Cities primorsko done.

--- resources/views/components/page-wrap-primorsko.blade.php
+++ resources/views/components/page-wrap-primorsko.blade.php
@@ -7,9 +7,9 @@
                     <div class="well">
                         <h3>{!! trans('home.cities') !!}</h3>
                       <div class="list-group">
-                        <a class="list-group-item" href="/pomorie"> <h4 class="list-group-item-heading">{!! trans('home.pomorie_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.pomorie_house_name') !!}</p> </a>
-                        <a class="list-group-item" href="/sinemorets"> <h4 class="list-group-item-heading">{!! trans('home.sinemorets_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.sinemorets_house_name') !!}</p> </a>
-                        <a class="list-group-item active" href="/primorsko"> <h4 class="list-group-item-heading">{!! trans('home.primorsko_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.primorsko_house_name') !!}</p> </a>
+                        <a class="list-group-item" href="/cities#pomorie"> <h4 class="list-group-item-heading">{!! trans('home.pomorie_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.pomorie_house_name') !!}</p> </a>
+                        <a class="list-group-item" href="/cities#sinemorets"> <h4 class="list-group-item-heading">{!! trans('home.sinemorets_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.sinemorets_house_name') !!}</p> </a>
+                        <a class="list-group-item active" href="/cities#primorsko"> <h4 class="list-group-item-heading">{!! trans('home.primorsko_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.primorsko_house_name') !!}</p> </a>
                       </div>
                     </div>
 </div>
