6f704d9b6198c8c01c1ee071169dafadfe8cc389
VeselinKichukov <veskichukov@gmail.com>
2018-12-04 21:24:51 +0100

    Change page-wrap and primorsko.

--- resources/views/cities/primorsko.blade.php
+++ resources/views/cities/primorsko.blade.php
@@ -41,27 +41,22 @@
 
     <div class="col-lg-3  col-md-3 col-sm-12">
         <div class="well">
-            <h3>Appartments in Primorsko</h3>
-            <div>
-                <div class="row"><a href="#">
-                        <img data-src="holder.js/64x64"
-                             class="media-object"
-                             alt="64x64"
-                             width='100%'
-                             height='auto'
-                             src="{{ asset('../img/blog/pm_house.jpg') }}"
-                             data-holder-rendered="true"> </a></div>
-                <div class="row" style="text-align: center; display: block;">
-                    <h4 class="media-heading">Guest House More</h4>
-                    In the centre of Pomorie and only 50 metres away from the beach.
+            <h3>{!! trans('home.primorsko_appartments') !!}</h3>
+            <div class="media">
+                <div class="media-left"><a href="#"> <img data-src="../img/blog/primosrsko_house.jpg"
+                                                          class="media-object"
+                                                          alt="64x64" width='200px' height='200px'
+                                                          src="{{ asset('../img/blog/primosrsko_house.jpg') }}"
+                                                          data-holder-rendered="true"> </a></div>
+                <div class="media-body"><h5 class="media-heading">{!! trans('home.primorsko_house_name') !!}</h5>{!!
+                trans('home.primorsko_short') !!}
                 </div>
             </div>
-            <!-- <div class="media"> <div class="media-left"> <a href="#"> <img data-src="holder.js/64x64" class="media-object" alt="64x64" style="width: 64px; height: 64px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTY5MjIxZTM1NSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NjkyMjFlMzU1Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"> </a> </div> <div class="media-body"> <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla.</div> </div>
-            <div class="media"> <div class="media-left"> <a href="#"> <img data-src="holder.js/64x64" class="media-object" alt="64x64" style="width: 64px; height: 64px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTY5MjIxZTM1NSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NjkyMjFlMzU1Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMi41IiB5PSIzNi44Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true"> </a> </div> <div class="media-body"> <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla.</div> </div> -->
+
         </div>
+
+        @include('components.page-wrap')
+
     </div>
-
-    @include('components.page-wrap')
-
 
 </div>
--- resources/views/components/page-wrap.blade.php
+++ resources/views/components/page-wrap.blade.php
@@ -1,19 +1,10 @@
 <div class="col-lg-3  col-md-3 col-sm-12">
     <div id="nav" class="well">
-        <h3>Cities:</h3>
+        <h3>{!! trans('home.cities') !!}</h3>
         <div class="list-group">
-            <a id="pomorie-tab" class="list-group-item" href="#pomorie">
-                <h4 class="list-group-item-heading"> Pomorie</h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus.
-                                                Maecenas sed diam eget risus varius blandit.</p></a>
-            <a id="sinemorets-tab" class="list-group-item" href="#sinemorets">
-                <h4 class="list-group-item-heading">Sinemorets</h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus.
-                                                Maecenas sed diam eget risus varius blandit.</p></a>
-            <a id="primorsko-tab" class="list-group-item" href="#primorsko">
-                <h4 class="list-group-item-heading">Primorsko</h4>
-                <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus.
-                                                Maecenas sed diam eget risus varius blandit.</p></a>
+            <a class="list-group-item" href="/cities#pomorie"> <h4 class="list-group-item-heading">{!! trans('home.pomorie_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.pomorie_house_name') !!}</p> </a>
+            <a class="list-group-item" href="/cities#sinemorets"> <h4 class="list-group-item-heading">{!! trans('home.sinemorets_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.sinemorets_house_name') !!}</p> </a>
+            <a class="list-group-item active" href="/cities#primorsko"> <h4 class="list-group-item-heading">{!! trans('home.primorsko_name') !!}</h4> <p class="list-group-item-text">{!! trans('home.primorsko_house_name') !!}</p> </a>
         </div>
     </div>
 </div>
