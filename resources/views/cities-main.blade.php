781054c5377fb1f2126f2a6df296d7c860a53a56
Georgi Serbezov <george.serbezov@gmail.com>
2018-12-13 21:13:49 +0100

    Language fix

--- resources/views/cities-main.blade.php
+++ resources/views/cities-main.blade.php
@@ -57,7 +57,11 @@
               href="https://use.fontawesome.com/releases/v5.4.2/css/all.css"
               integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns"
               crossorigin="anonymous">
-
+              <style type="text/css" media="screen">
+                .navbar-nav>li>a {
+                    padding-top: 0px !important;
+                }
+              </style>   
         <!-- blog template -->
         <link rel="stylesheet" type="text/css" href="css/pomorie.css">
         <!------ Include the above in your HEAD tag ---------->
